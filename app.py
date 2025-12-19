9a5c73ea1522f96bec15b4ea4707b41146136088
Nawin M S <ramelams07@gmail.com>
2025-12-19 20:57:03 +0530

    FINAL RENDER

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def home():
     return render_template("index.html", response = response)
     
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
